eca209d7f4270e13a133eb851f605b476a00e99f
Quentin Bauduin <bauduinquentinn@gmail.com>
2024-11-07 18:52:19 +0100

    💄 fix: background color ne prennait pas la bonne hauteur

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={`${dmSans.className} bg-slate-50/30 text-slate-800`}>
+      <body
+        className={`${dmSans.className} min-h-screen bg-slate-50/30 text-slate-800`}
+      >
         <div className="mx-auto max-w-[960px]">
           <Header />
           <main className="my-4 px-8">{children}</main>
